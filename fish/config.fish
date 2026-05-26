7172a9ed085013e64093374812deb0e74cfeceb8
randomniht <11s13tayr@gmail.com>
2026-05-26 21:48:24 +0500

    ADD_scr

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,27 @@ set -U fish_greeting ""
  fish_add_path ~/.config/emacs/bin/
  fish_add_path ~/.local/bin/
 
+function wgup
+    sudo wg-quick up $argv[1]
+end
+
+function wgdw
+    sudo wg-quick down $argv[1]
+end
+
+function v2a
+    echo "Запуск служб v2rayA..."
+    sudo systemctl start v2raya xray
+    sleep 1
+    echo "Panel now on http://localhost:2017 (miht)"
+end
+function v2as
+    echo "Остановка служб v2rayA..."
+    sudo systemctl stop v2raya xray
+    echo "Прокси выключен."
+end
+
+
 alias l="ls -lh"
 alias ls="exa"
 alias nivm="nvim"
@@ -27,3 +48,4 @@ alias upwall="sudo ./bash_fish_skr/wallpaper.sh"
 alias fastfetch='fastfetch -c 28.jsonc'
 alias ft='clear && fastfetch'
 alias muxff="$HOME/dotfiles/scripts/mux_vlc/mux_inter.fish"
+alias ipnow="curl ipinfo.io"
